42ea6f0699ceebe5f678b3e2f6b5029574545cd8
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-09-23 16:10:07 -0700

    added timer, card gets pulled every 2 seconds

--- src/CardTable.js
+++ src/CardTable.js
@@ -9,24 +9,35 @@ class CardTable extends Component {
     this.state = {
       deckId: '',
       faceUpCards: [],
-      cardsAvailable: true
+      cardsAvailable: true,
+      timerId: ''
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   async componentDidMount() {
     let result = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-    this.setState({ deckId: result.data.deck_id });
+    let timerId = setInterval(this.handleClick, 2000)
+    this.setState({ deckId: result.data.deck_id, timerId });
   }
 
   async handleClick() {
     let result = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=1`);
     let cardsAvailable = result.data.remaining > 0 ? true : false;
     console.log(result.data)
-    this.setState({
-      faceUpCards: [...this.state.faceUpCards, result.data.cards[0]],
-      cardsAvailable
-    });
+    // handle case to clear interval after no cards remain.
+    if(cardsAvailable){
+      this.setState({
+        faceUpCards: [...this.state.faceUpCards, result.data.cards[0]],
+        cardsAvailable
+      });
+    } else {
+      clearInterval(this.state.timerId);
+      this.setState({
+        cardsAvailable,
+        timerId: ''
+      });
+    }
   }
 
   render() {
